9e8afe9c032986dbf3679d8076e9430e70a30803
ahmedtehseen <ahmedpisces7@gmail.com>
2016-12-04 22:40:33 +0500

    addednewPost component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+import fetchPosts from '../actions/index';
+
+class PostsIndex extends Component {
+	componentWillMount(){
+		this.props.fetchPosts();
+	}
+
+	render(){
+		return(
+			<div>List of blog posts</div>
+		);
+	}
+}
 
 
-export default ()=>{
-	return <div>List aof blog posts</div>;
-};
+export default connect(null, { fetchPosts })(PostsIndex);
