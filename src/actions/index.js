a26658eabe8c7e8cc3e34d4f64d919e384f503f9
ahmedtehseen <ahmedpisces7@gmail.com>
2016-12-15 23:56:27 +0500

    added Redux Form with validation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=ljsldkfslfnkvjs';
+const API_KEY = '?key=lasjdlksajdlkasj';
 
 
-export default function fetchPosts(){
-	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);	
+function fetchPosts(){
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 	return{
 		type: FETCH_POSTS,
 		payload: request
 	};
 }
+
+function createPost(props){
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+	return{
+		type: CREATE_POST,
+		payload: request
+	};	
+}
+
+export default (fetchPosts, createPost);
